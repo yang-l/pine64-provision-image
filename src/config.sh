eb6f267d1f2aaa416f85079b26b57b74399f3c30
YL <Y@L>
2016-08-06 17:54:15 +1200

    Init commit to use custom kernel during the pervision stage

--- src/config.sh
+++ src/config.sh
@@ -1,7 +1,7 @@
 BASE_IMAGE_URL="https://www.stdin.xyz/downloads/people/longsleep/pine64-images/simpleimage-pine64-latest.img.xz"
 BASE_IMAGE="pine64.img"
 
-KERNEL_URL="https://www.stdin.xyz/downloads/people/longsleep/pine64-images/linux/linux-pine64-latest.tar.xz"
+KERNEL_URL="https://github.com/yang-l/pine64-kernel/raw/init-bootstrap/kernel.tar.xz"
 KERNEL_FILE="kernel.tar.xz"
 
 SRC_DIR="$(cd -P "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
