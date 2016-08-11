967d906c3a600864d4f468b9efb47cf26e522e37
YL <Y@L>
2016-08-11 21:52:57 +1200

    Updated to use docker v1.12

--- src/docker-debian.sh
+++ src/docker-debian.sh
@@ -1,13 +1,9 @@
 RUNLEVEL=1 apt-get -y -t jessie-backports --no-install-recommends install libseccomp2 aufs-tools xz-utils cgroupfs-mount git ca-certificates
 
-DOCKER_VER="v1.12.0-rc4"
+DOCKER_VER="v1.12.0"
 
 # use prebuild docker for aarch64
-for pkg in docker docker-containerd docker-containerd-ctr docker-containerd-shim docker-proxy docker-runc dockerd
-do
-    curl -sLk https://github.com/yang-l/pine64-docker/raw/"${DOCKER_VER}"/bin/"${pkg}" -o /usr/bin/"${pkg}"
-    chmod 755 /usr/bin/"${pkg}"
-done
+curl -sLk https://github.com/yang-l/pine64-docker/raw/"${DOCKER_VER}"/docker.tar.xz | tar -C /usr/bin/ -xJpf -
 
 # setup systemd unit files
 curl -sLk https://github.com/docker/docker/raw/"${DOCKER_VER}"/contrib/init/systemd/docker.service -o /etc/systemd/system/docker.service
